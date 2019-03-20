13310d614ad72bb29f67d0207f10aa4237fc0169
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-03-20 15:12:44 -0600

    Adds view for books index - stats tests pass

--- spec/features/books_index_spec.rb
+++ spec/features/books_index_spec.rb
@@ -7,25 +7,25 @@ RSpec.describe "a user visiting books index page" do
   context "when I look at the stats bar" do
     it "should have 3 highest rated books" do
       visit books_path
-      within ".statistics" do
-        expect(page).to have_content("Three highest reviewed books:")
+      within ".highest-rated" do
+        expect(page).to have_content("Three highest rated books:")
         expect(page).to have_css(".book-snippet", count: 3)
       end
     end
 
     it "should have 3 lowest rated books" do
       visit books_path
-      within ".statistics" do
-        expect(page).to have_content("Three lowest reviewed books:")
-        expect(page).to have_css(".book-snippet", count: 3)
+      within ".lowest-rated" do
+        expect(page).to have_content("Three lowest rated books:")
+        expect(page).to have_css("div.book-snippet", count: 3)
       end
     end
 
     it "should have 3 users who have reviewed the most books" do
       visit books_path
-      within ".statistics" do
+      within ".top-reviewers" do
         expect(page).to have_content("Top Reviewers")
-        expect(page).to have_css(".user-snippet", count: 3)
+        expect(page).to have_css("div.user-snippet", count: 3)
       end
     end
   end
