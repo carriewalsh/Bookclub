1880c588f675d02b091349d4653b256b4e4a5182
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-03-20 12:33:22 -0600

    Creates testes for book index nav bar

---
+++ spec/features/books_index_spec.rb
@@ -0,0 +1,31 @@
+require "rails_helper"
+
+RSpec.describe " a user visiting the index page" do
+  before :each do
+  end
+
+
+  context "when I look at the navigation bar" do
+    it "should have the links" do
+      visit root_path
+      within "nav" do
+        expect(page).to have_link("Home")
+        expect(page).to have_link("Books")
+      end
+    end
+
+    it "should link home to root" do
+      within "nav" do
+        click_link "Home"
+        expect(current_path).to eq root_path
+      end
+    end
+
+    it "should link books to book index page" do
+      within "nav" do
+        click_link "Books"
+        expect(current_path).to eq books_path
+      end
+    end
+  end
+end
