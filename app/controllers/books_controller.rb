e77f8948d41f6b560f8c9ebbde75db8322283f3d
Ethan Grab <ethangrab@gmail.com>
2019-03-20 23:41:32 -0600

    BooksController#show

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,10 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
   end
+
+  def show
+    @book = Book.find(params[:id])
+    @authors = @book.authors
+    @reviews = @book.reviews
+  end
 end
