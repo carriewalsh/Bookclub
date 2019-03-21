b63355096a0149c648a5e390cde8e0144c442741
Ethan Grab <ethangrab@gmail.com>
2019-03-21 12:49:51 -0600

    User story 11 functinality complete

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,8 +7,8 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @authors = @book.authors
     @reviews = @book.reviews
-    @top_three_reviews = @reviews.order(:rating).take(3)
-    @worst_three_reviews = @reviews.order(rating: :desc).take(3)
+    @worst_three_reviews = @reviews.order(:rating).take(3)
+    @top_three_reviews = @reviews.order(rating: :desc).take(3)
     @average_review_rating = @reviews.average(:rating)
   end
 
