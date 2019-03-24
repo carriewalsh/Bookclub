0a578f011b3a9633372dc17bf1f3e4551b4a079c
Ethan Grab <ethangrab@gmail.com>
2019-03-24 17:42:56 -0600

    Added link to user show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "welcome#index"
 
-  resources :users, only [:show]
-
   resources :books, only: [:index, :show, :destroy, :create, :new] do
-    resources :reviews, only: [:new, :create]
+    resources :reviews, only: [:new, :create, :show]
   end
 
 end
