c57048c271655ebda34d661afca7cbd3fd2465a1
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-04-23 09:50:03 -0600

    Reviews routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "welcome#index"
+
+  # original routes that have path_helpers
   resources :reviews, only: [:show, :destroy]
 
   resources :books, only: [:index, :show, :destroy, :create, :new] do
@@ -9,4 +11,19 @@ Rails.application.routes.draw do
 
   resources :authors, only: [:show, :destroy]
 
+  # new - handrolled routes for prework practice
+  # get "/reviews/:id", to: "reviews#show"
+  # delete "/reviews/:id", to: "reviews#destroy"
+  #
+  # get "/books", to: "books#index"
+  # get "/books/:id", to: "books#show"
+  # delete "/books/:id", to: "books#destroy"
+  # post "/books", to: "books#create"
+  # get "/books/new", to: "books#new"
+  #
+  # get "/books/:book_id/reviews/new", to: "reviews#new"
+  # post "/books/:book_id/reviews", to: "reviews#create"
+  #
+  # get "/authors/:id", to: "authors#show"
+  # delete "/authors/:id", to: "authors#destroy"
 end
